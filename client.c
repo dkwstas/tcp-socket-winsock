da642a9ba61b94dc89a92e394e2d37f6bcd58163
Kostas <gedra100sh@gmail.com>
2024-04-23 04:33:49 +0300

    Initial commit

--- client.c
+++ client.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
-#define IP "0.0.0.0"
 #define MAX_LENGTH 80
 #define WIN32_LEAN_AND_MEAN
 
@@ -13,15 +12,16 @@ void com (int conn) {
 
     while (1) {
         memset(buffer, '\0', MAX_LENGTH);
+        printf("TX: ");
+        scanf("%s", buffer);
+        send(conn, buffer, MAX_LENGTH, 0);
+
+        memset(buffer, '\0', MAX_LENGTH);
         if (recv(conn, buffer, MAX_LENGTH, 0) <= 0) {
             printf("Connection lost.\n");
             break;
         }
-        printf("RX: %s\nTX: ", buffer);
-
-        memset(buffer, '\0', MAX_LENGTH);
-        scanf("%s", buffer);
-        send(conn, buffer, MAX_LENGTH, 0);
+        printf("RX: %s\n", buffer);
     }
 }
 
@@ -31,14 +31,14 @@ void free_heap (struct sockaddr_in *sock_addr, struct WSAData *wsadata) {
 }
 
 int main (int argc, char *argv[]) {
-    int bind_status = -1, listen_status = -1, wsa_status = -1;
+    int conn_status = -1, wsa_status = -1;
     struct in_addr ip_addr;
-    SOCKET sock, conn;
+    SOCKET sock;
     struct sockaddr_in *sock_addr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
     struct WSAData *wsadata = (struct WSAData *)malloc(sizeof(struct WSAData));
 
-    if (argc != 2) {
-        printf("Usage ./server <PORT>\n");
+    if (argc != 3) {
+        printf("Usage ./client <IP> <PORT>\n");
         free_heap(sock_addr, wsadata);
         return(-1);
     }
@@ -53,39 +53,24 @@ int main (int argc, char *argv[]) {
     //Creating TCP socket
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     //Defining IP in the structure
-    ip_addr.S_un.S_addr = inet_addr(IP);
+    ip_addr.S_un.S_addr = inet_addr(argv[1]);
     //Setting IP, PORT, SOCKET TYPE
     sock_addr->sin_family = AF_INET;
     sock_addr->sin_addr = ip_addr;
-    sock_addr->sin_port = htons(atoi(argv[1]));
-
-    //Binding address
-    bind_status = bind(sock, (struct sockaddr *)sock_addr, sizeof(*sock_addr));
-    if (bind_status != 0) {
-        printf("Error binding address: %d\n", bind_status);
-        return(bind_status);
-    }
-
-    //Starting listener with no queue
-    listen_status = listen(sock, -1);
-    if (listen_status != 0) {
-        printf("Error starting listener: %d\n", listen_status);
-        return(listen_status);
-    }
+    sock_addr->sin_port = htons(atoi(argv[2]));
 
     //Accepting incoming connection
-    conn = accept(sock, NULL, NULL);
+    conn_status = connect(sock, (struct sockaddr *)sock_addr, sizeof(*sock_addr));
 
-    if (conn == INVALID_SOCKET) {
-        printf("Client not accepted\n");
+    if (conn_status == SOCKET_ERROR) {
+        printf("Can't connect\n");
     } else {
-        printf("Client accepted\n");
-        com(conn);
+        printf("Connected\n");
+        com(sock);
     }
 
     shutdown(sock, SD_BOTH);
     closesocket(sock);
-    closesocket(conn);
 
     WSACleanup();
 
